d5e12efee8204b286723aa11f580d48d08c28463
tungufoss <helgaingim@hi.is>
2024-11-08 12:22:57 +0000

    Allow for special stiches via footnotes

--- main.py
+++ main.py
@@ -5,15 +5,25 @@ from knitviz.pattern_parser import process_pattern
 
 def main():
     parser = argparse.ArgumentParser(description="Parse and visualize a knitting pattern.")
-    parser.add_argument('--mapping', default='data/stitch_mapping.json', help="Path to the JSON stitch mapping file")
-    parser.add_argument('--pattern', default='data/devil_helmet.json', help="Path to the JSON knitting pattern file")
-    parser.add_argument('--output_dir', default='figures', help="Directory to save output images")
+    parser.add_argument('--mapping', default='data/stitch_mapping.json',
+                        help="Path to the JSON stitch mapping file")
+    parser.add_argument('--pattern', default='data/devil_helmet.json',
+                        help="Path to the JSON knitting pattern file")
+    parser.add_argument('--output_dir', default='figures',
+                        help="Directory to save output images")
     args = parser.parse_args()
 
     os.makedirs(args.output_dir, exist_ok=True)
-    stitch_mapping = load_json(args.mapping)
-    pattern_data = load_json(args.pattern)
 
+    # Load the pattern data and stitch mapping
+    pattern_data = load_json(args.pattern)
+    stitch_mapping = load_json(args.mapping)
+
+    # Check if the pattern has footnotes, and if so, add them to the stitch mapping
+    if 'footnotes' in pattern_data:
+        stitch_mapping.update(pattern_data['footnotes'])
+
+    # Process each part of the pattern, in the order they are defined
     for part_name in pattern_data['parts']:
         part_data = pattern_data[part_name]
         name = os.path.splitext(os.path.basename(args.pattern))[0] + "_" + part_name
